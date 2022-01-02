9bab39c4ce11ad7cbe3e06058f0a4846e9a78c84
mbanani-git <leedbn1@gmail.com>
2022-01-02 20:31:07 +0100

    ok

--- controllers/controller.js
+++ controllers/controller.js
@@ -39,7 +39,7 @@ const postUrl = async (req, res, next) => {
       }
     });
   } catch (error) {
-    return res.status(500).json({ error: "Invalid URL" });
+    return res.status(500).json({ error: "invalid url" });
   }
 };
 
--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getUrl, postUrl, sendUrl } = require("../controllers/controller");
+const { getUrl, postUrl } = require("../controllers/controller");
 
 router.get("/api/shorturl/:url", getUrl);
 router.post("/api/shorturl", postUrl);
